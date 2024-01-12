8db01c129c5a220b876787d9e4976925276bcbd7
Colten Mikulastik <mikulastikc@gmail.com>
2024-01-12 16:28:11 -0600

    starting repository, basic functionality

---
+++ __main__.py
@@ -0,0 +1,48 @@
+
+import requests
+import json
+
+
+def abuseIPDB_API_Call(ip_address):
+    """ uses abuseIPDB_API_Call gets JSON """
+    # unpack my api key
+    # might require fix later
+    with open("AbuseIPDB_API_Key.key", 'r') as file:
+        key = file.read()
+        key = key.split()
+        key = "".join(key)
+
+    # set parameters for the API request
+    params = {
+        "ipAddress": ip_address
+    }
+    headers = {
+        "Key": key,
+        "Accept": "application/json"
+    }
+
+    # call api
+    api_response = requests.get("https://api.abuseipdb.com/api/v2/check",
+                                params=params,
+                                headers=headers)
+
+    # format from byte thing to json dict
+    json_resp = json.loads(api_response.content.decode("utf-8"))
+
+    for key, value in json_resp["data"].items():
+        print(str(key) + ": " + str(value))
+
+
+def main():
+    """ gets user input and calls all the other functions """
+    print("===========================")
+    print("ip_columbo (the ip checker)")
+    print("===========================")
+
+    ip_address = input("ip: ")
+    print(ip_address)
+    abuseIPDB_API_Call(ip_address)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,24 @@
+
+# ip_columbo
+This program is created to allow security analysts to quickly and effectively search for information on IP addresses.
+
+## Requirements:
+
+### APIs
+ip_columbo is heavily relyant on API keys to these websites.
+    - ***AbuseIPDB***
+    - ***VirusTotal*** (coming soon)
+
+### ETC.
+- Python3
+    - requests
+    - json
+to install python requirements
+``` pip3 install -r requirements.txt ```
+
+## Features / Improvements / Bug-Fixes
+Standard github procedures:
+    - fork library to your own repos
+    - make changes
+    - make PR
+
